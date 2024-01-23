2d93b5d9e5466809bf57246e6d3a564f44b4bc57
Johan Runsten <johanrunsten86@gmail.com>
2024-01-23 23:58:50 +0100

    Add hyprland

--- home-manager/features/hyprland.nix
+++ home-manager/features/hyprland.nix
@@ -1,11 +1,4 @@
 { pkgs, lib, config, ... }: {
-  # home.packages = [ pkgs.hyprland ];
-  
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-
   wayland.windowManager.hyprland = {
     enable = true;
   };
