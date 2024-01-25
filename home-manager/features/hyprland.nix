ffbbd46205a3cc78c01ef48637d7edd5651bd1da
Johan Runsten <johanrunsten86@gmail.com>
2024-01-25 22:49:23 +0100

    Added neofetch

--- home-manager/features/hyprland.nix
+++ home-manager/features/hyprland.nix
@@ -2,8 +2,4 @@
   wayland.windowManager.hyprland = {
     enable = true;
   };
-
-  settings = {
-
-  };
 }
--- home-manager/jr-home.nix
+++ home-manager/jr-home.nix
@@ -12,18 +12,4 @@ in {
 
   #wallpaper = pkgs.wallpapers.aurora-borealis-water-mountain;
   #colorScheme = lib.mkDefault colorSchemes.tokyo-city-terminal-dark;
-
-  #  ------   -----   ------
-  # | DP-3 | | DP-1| | DP-2 |
-  #  ------   -----   ------
-  # monitors = [
-  #   {
-  #     name = "DP-1";
-  #     width = 2560;
-  #     height = 1080;
-  #     x = 1920;
-  #     workspace = "1";
-  #     primary = true;
-  #   }
-  # ];
 }
