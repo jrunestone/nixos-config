bfe20f812d2f7b68cbd10031cfe977a1127c7bcd
Johan Runsten <johanrunsten86@gmail.com>
2024-01-30 22:31:06 +0100

    Fix env vars

--- home-manager/features/fzf.nix
+++ home-manager/features/fzf.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, config, ... }: {
   home.packages = [ pkgs.fzf ];
 
-  home.sessionVariables = {
-    FZF_DEFAULT_COMMAND = "find \! \( -path '*/.git' -o -path '*/node_modules' -prune \) -printf '%P\n'";
+  programs.bash.sessionVariables = {
+    FZF_DEFAULT_COMMAND = "find \\! \\( -path '*/.git' -o -path '*/node_modules' -prune \\) -printf '%P\\n'";
   };
 }
