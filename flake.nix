f64b102890345fc0475810da56730acd42123d02
Johan Runsten <johanrunsten86@gmail.com>
2024-02-04 21:55:07 +0100

    Syntax

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.jr = import ./home-manager/jr-home.nix;
-              extraSpecialArgs = { inherit inputs outputs; };
+              home-manager.extraSpecialArgs = { inherit inputs outputs; };
             }
           ];
         };
@@ -62,7 +62,7 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.jr = import ./home-manager/jr-work.nix;
-              extraSpecialArgs = { inherit inputs outputs; };
+              home-manager.extraSpecialArgs = { inherit inputs outputs; };
             }
           ];
         };
