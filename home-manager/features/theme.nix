6ebfeae44a09c3fbea41959910a8ebee1a958a93
Johan Runsten <johanrunsten86@gmail.com>
2024-02-17 21:38:44 +0100

    Wallpaper fix

--- home-manager/features/theme.nix
+++ home-manager/features/theme.nix
@@ -7,6 +7,10 @@
     PATH="${config.home.path}/bin:$PATH" $DRY_RUN_CMD gradience-cli apply -n "Zorin OS 16 Green Dark"
   '';
 
+  home.file."${config.xdg.userDirs.documents}/wallpapers" = {
+    source = ../wallpapers;
+  };
+
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
@@ -20,13 +24,9 @@
       favorite-apps = []; # TODO: rider, slack, spotify, steam, ...
     };
 
-    home.file."${xdg.userDirs.documents}/wallpapers" = {
-      source = ../wallpapers;
-    };
-
     "org/gnome/desktop/background" = {
-      picture-uri = "file://${xdg.userDirs.documents}/wallpapers/1.jpg";
-      picture-uri-dark = "file://${xdg.userDirs.documents}/wallpapers/1.jpg";
+      picture-uri = "file://${config.xdg.userDirs.documents}/wallpapers/1.jpg";
+      picture-uri-dark = "file://${config.xdg.userDirs.documents}/wallpapers/1.jpg";
     };
   };
 }
