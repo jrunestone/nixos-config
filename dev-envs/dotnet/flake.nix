bce3d70fa4acf5cf21ceccf95df35ece56a1dd24
Johan Runsten <johanrunsten86@gmail.com>
2024-02-24 19:34:41 +0100

    Basic dev envs

---
+++ dev-envs/dotnet/flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    system = "x86_64-linux";    
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        (with dotnetCorePackages; combinePackages [
+          sdk_8_0
+        ])
+        jetbrains.rider
+      ];
+
+      shellHook = ''
+        echo "Ready"
+        trap 'echo "Bye"' EXIT
+      '';
+    };
+  };
+}
---
+++ dev-envs/empty/flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    system = "x86_64-linux";    
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+      ];
+
+      shellHook = ''
+        echo "Ready"
+        trap 'echo "Bye"' EXIT
+      '';
+    };
+  };
+}
