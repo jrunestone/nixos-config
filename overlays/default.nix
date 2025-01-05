a02c4ce1a59d49d7d9f9c826eaf83b95c6e54729
Johan Runsten <johan85@hotmail.com>
2025-01-05 13:42:15 +0100

    VS

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,10 +6,10 @@
     # ...
     # });
     vintagestory = prev.vintagestory.overrideAttrs (old: rec {
-      version = "1.20.0-rc.6";
+      version = "1.20.0-rc.8";
       src = prev.fetchurl {
         url = "https://cdn.vintagestory.at/gamefiles/unstable/vs_client_linux-x64_${version}.tar.gz";
-        sha256 = "sha256-JxyQrZrAmMkUy28JUak0BHHpuGEb2fbxNrtOGVKGhOQ=";
+        sha256 = "sha256-/MPR6PAkZv93zT6YbJatg67aRYfzp9vFRY82gtVksAs=";
       };
     });
   };
