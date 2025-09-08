a748bd2838c35dda7af1f4a2f12405aaafc0076b
Johan Runsten <johan85@hotmail.com>
2025-09-08 17:36:27 +0200

    Keys

--- hosts/jr-homelab/features/system/ssh.nix
+++ hosts/jr-homelab/features/system/ssh.nix
@@ -7,13 +7,12 @@
       AllowUsers = ["jr"];
       PasswordAuthentication = false;
     };
-
-    #knownHosts = {
-    #  "jr-home" = {
-    #    publicKey = "";
-        
-    #};
   };
+
+  users.users.jr.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAt6S7VQGYS2X2RM44KNcbvtCbrTURZgIEWUOtrzQ26T johan85@hotmail.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII24IDUM7AHXVxtXU6P0lQ4gn4FEvUPwOIjDutuH4Rp+ johan.runsten@toxic.se"
+  ];
 
   networking.firewall.allowedTCPPorts = [22];
 }
