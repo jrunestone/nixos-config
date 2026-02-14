faa169752be8f01444ca82fa3f15822422445439
Johan Runsten <jrunestone@users.noreply.github.com>
2026-02-14 16:08:15 +0100

    Update post-create.sh

--- dev-envs/devcontainer-dotnet/.devcontainer/post-create.sh
+++ dev-envs/devcontainer-dotnet/.devcontainer/post-create.sh
@@ -2,7 +2,3 @@
 
 # this is run on "postCreateCommand" in the container AFTER the container is CREATED for the first time
 # https://containers.dev/implementors/json_reference/
-
-# set correct envs if we are running in codespace
-#if [ "$CODESPACES" = true ]; then
-#fi
