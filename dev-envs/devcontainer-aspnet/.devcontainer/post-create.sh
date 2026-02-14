a3f0642d7d6e35565c54f7816b18b2753cd5423d
Johan Runsten <jrunestone@users.noreply.github.com>
2026-02-14 16:04:22 +0100

    Update post-create.sh

--- dev-envs/devcontainer-aspnet/.devcontainer/post-create.sh
+++ dev-envs/devcontainer-aspnet/.devcontainer/post-create.sh
@@ -3,13 +3,28 @@
 # this is run on "postCreateCommand" in the container AFTER the container is CREATED for the first time
 # https://containers.dev/implementors/json_reference/
 
-# create a dev https cert if not already present
-if ! [ -f ./.devcontainer/localhost.pfx ]; then
+DEVC_DIR=./.devcontainer
+
+# if the image doesn't load ip_tables modules dockerd will fail to launch (docker-in-docker)
+# https://github.com/devcontainers/features/issues/1235
+echo "Updating iptables"
+sudo update-alternatives --set iptables /usr/sbin/iptables-nft
+
+# create a dev https cert if a custom cert is not already present
+if ! [ -f $DEVC_DIR/localhost.pfx ]; then
+  echo "Creating default ASP.NET dev cert"
   dotnet dev-certs https --trust
-  dotnet dev-certs https -ep ./.devcontainer/localhost.pfx -p 'changeit'
-  chmod +w ./.devcontainer/localhost.pfx
+  dotnet dev-certs https -ep $DEVC_DIR/localhost.pfx -p 'changeit'
 fi
 
-# set correct envs if we are running in codespace
-#if [ "$CODESPACES" = true ]; then
-#fi
+# copy and trust any custom cert to the ca store
+if [ -f $CUSTOM_CA_FILE ]; then
+  echo "Trusting custom CA cert $CUSTOM_CA_FILE"
+  sudo cp $CUSTOM_CA_FILE /usr/local/share/ca-certificates/
+  sudo update-ca-certificates
+fi
+
+# do things if we are running in codespace
+if [ "$CODESPACES" = true ]; then
+  echo "Running in a Codespace"
+fi
