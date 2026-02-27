fe8b73f2ca861982f6c23beab44adb6a7b7657b7
Johan Runsten <johan.runsten@toxic.se>
2026-02-27 16:49:29 +0100

    Add devcontainer-c

---
+++ dev-envs/devcontainer-c/.devcontainer/post-create.sh
@@ -0,0 +1,13 @@
+sudo apk update 
+sudo apk upgrade 
+sudo apk add --no-cache \
+    clang \
+    clang-dev \
+    gdb \
+    valgrind \
+    alpine-sdk \
+    dpkg \
+    cmake \
+    ccache \
+    python3 \
+    ninja
